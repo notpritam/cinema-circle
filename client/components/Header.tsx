f5e0122c08aa28e06e7c1df8f84857060681c17e
Pritam Sharma <notpritamsharma@gmail.com>
2024-07-13 17:04:01 +0530

    Landing Page

---
+++ client/components/Header.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import logoImage from "@/public/images/logo.svg";
+import searchImage from "@/public/images/search.svg";
+import menuImage from "@/public/images/menu.svg";
+import Image from "next/image";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+interface Link {
+  name: string;
+  url: string;
+}
+
+function Header() {
+  const links: Link[] = [
+    { name: "Movies", url: "/" },
+    { name: "Stream", url: "/about" },
+    { name: "Events", url: "/contact" },
+    { name: "Plays", url: "/contact" },
+    { name: "Sports", url: "/contact" },
+    { name: "Activites", url: "/contact" },
+  ];
+  return (
+    <header className="flex flex-col">
+      <div className="w-full  h-[64px] items-center justify-center flex">
+        <div className="container py-4 flex gap-8 justify-between">
+          <div className="flex gap-8">
+            <Image src={logoImage} alt="logo" />
+            <div className="flex bg-white px-4 min-w-[400px] border-gray-300 border-[1px] rounded-sm">
+              <Image src={searchImage} alt="logo" />
+              <Input
+                placeholder="Search"
+                className="bg-transparent border-none shadow-none"
+              />
+            </div>
+          </div>
+
+          <div className="flex gap-4 items-center">
+            <Button className="bg-showRed">Sign in</Button>
+
+            <Button className="bg-transparent">
+              <Image src={menuImage} alt="Menu Button" />
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className="w-full bg-lightGray border-showBorder border-b-[1px] h-[40px] flex items-center">
+        <div className="container flex justify-between text-sm">
+          <div className="flex gap-4 items-center">
+            {links.map((link, index) => (
+              <>
+                <Link key={index} href={link.url}>
+                  {link.name}
+                </Link>
+              </>
+            ))}
+          </div>
+          <div className="flex gap-4 items-center">
+            {links.map((link, index) => (
+              <>
+                <Link key={index} href={link.url}>
+                  {link.name}
+                </Link>
+              </>
+            ))}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ client/components/SectionTitle.tsx
@@ -0,0 +1,30 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
+
+interface SectionTitleProps {
+  title: string;
+  link: string;
+  linkText: string;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+function SectionTitle({
+  title,
+  link,
+  linkText,
+  children,
+  className,
+}: SectionTitleProps) {
+  return (
+    <div className={cn("flex justify-between w-full items-center", className)}>
+      <h2 className="text-3xl font-bold">{title}</h2>
+      <Link className="text-showRed" href={link}>
+        {linkText}
+      </Link>
+    </div>
+  );
+}
+
+export default SectionTitle;
